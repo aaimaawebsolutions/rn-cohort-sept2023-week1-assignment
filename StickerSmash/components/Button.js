9535f2d083484ac2a9a36e7cdc7a3df1ab41c9e0
Alishan Shaikh <alishanshaikh9890@gmail.com>
2023-09-21 14:18:00 +0530

    bounce back

--- StickerSmash/components/Button.js
+++ StickerSmash/components/Button.js
@@ -1,31 +1,31 @@
 import { StyleSheet, View, Pressable, Text } from 'react-native';
-import { FontAwesome } from "@expo/vector-icons"; // Correct import statement
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
-export default function Button({ label, theme }) {
-  if (theme === "primary") {
-    return (
-      <View
-        style={[styles.buttonContainer, { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 }]}
-      >
-        <Pressable
-          style={[styles.button, { backgroundColor: "#fff" }]}
-          onPress={() => alert('You pressed a button.')}
-        >
-          <FontAwesome
-            name="picture-o"
-            size={18}
-            color="#25292e"
-            style={styles.buttonIcon}
-          />
-          <Text style={[styles.buttonLabel, { color: "#25292e" }]}>{label}</Text>
-        </Pressable>
-      </View>
-    );
-  }
+export default function Button({ label, theme, onPress }) {
+    if (theme === "primary") {
+        return (
+          <View
+          style={[styles.buttonContainer, { borderWidth: 4, borderColor: "#87cefa", borderRadius: 10 }]}
+          >
+            <Pressable
+              style={[styles.button, { backgroundColor: "#87cefa" }]}
+              onPress={onPress}
+            >
+              <FontAwesome
+                name="picture-o"
+                size={18}
+                color="#25292e"
+                style={styles.buttonIcon}
+              />
+              <Text style={[styles.buttonLabel, { color: "#25292e" }]}>{label}</Text>
+            </Pressable>
+        </View>
+        );
+    }
 
   return (
     <View style={styles.buttonContainer}>
-      <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+      <Pressable style={styles.button} onPress={onPress}>
         <Text style={styles.buttonLabel}>{label}</Text>
       </Pressable>
     </View>
